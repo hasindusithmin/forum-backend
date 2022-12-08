937fd9be34c95eb82a18a4b6ca1695bf006b16ef
hasindusithmin <hasindusithmin64@gmail.com>
2022-12-08 23:32:21 +0530

    Create routes/forum.js

---
+++ routes/forum.js
@@ -0,0 +1,18 @@
+
+const {Router} = require('express')
+const songModel = require('../models/song')
+
+const forumRoute = Router()
+
+forumRoute.get('/',(req,res)=>{
+    res.send({detail:'Use this endpoint to interact with forum.'})
+})
+
+forumRoute.get('/song',async(req,res)=>{
+    try {
+        const songs = await songModel.find()
+        res.status(200).json(songs)
+    } catch (error) {
+        res.status(500).json({error:error.message})
+    }
+})

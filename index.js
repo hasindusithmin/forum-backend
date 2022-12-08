027581804e6e3e1845c1c853abda85aa9c49e177
hasindusithmin <hasindusithmin64@gmail.com>
2022-12-08 22:49:35 +0530

    Update index.js(connect mongodb)

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-// install express with `npm install express` 
+
 const express = require('express')
+const mongoose = require('mongoose')
+
+// Create application object 
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/',(req,res)=>{
+    res.json(req.headers)
+})
 
-// export 'app'
-module.exports = app
+mongoose.set('strictQuery',true)
+mongoose.connect(process.env.SRV)
+    .then(()=>{
+        app.listen(3000,()=>{
+            console.log('db connect!\nserver runing port:3000');
+        })
+    })
+    .catch((error)=>{
+        console.error('ERROR',error.message);
+    })
+
+module.exports = app;
